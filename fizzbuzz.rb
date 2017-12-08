f0a7c093f434530c3ffcdeb41db9e9b6b12a99a3
Mahendra Pamidi <mahendrapamidi96@gmail.com>
2017-12-08 16:29:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(int)
-  if int % 3==0
-    "Fizz"
-  if int % 5==0
-    "Buzz"
   if int % 15==0
     "FizzBuzz"
+  elsif int % 3==0
+    "Fizz"
+  elsif int % 5==0
+    "Buzz"
+  
   end
 
 end
